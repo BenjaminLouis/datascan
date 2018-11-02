3da3c5a0033f04dcf82339437a0879bfb4377955
Benjamin Louis <benjaminlouis.pro@gmail.com>
2018-11-02 10:13:44 +0100

    :white_check_mark: test(scan_distributions.R) : update

--- tests/testthat/test-scan_distributions.R
+++ tests/testthat/test-scan_distributions.R
@@ -3,7 +3,12 @@ context("test-scan_distributions.R")
 test_that("scan-distributions works", {
 
   data(iris)
-  res <- scan_distributions(iris)
 
+  # Test if there are numeric variables
+  res <- scan_distributions(iris)
   expect_is(res, class = "data.frame")
+
+  # Test if there is not numeric variable
+  expect_error(scan_distributions(iris["Species"]),
+               "No numeric variables found")
 })
