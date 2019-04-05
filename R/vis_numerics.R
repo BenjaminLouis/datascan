d66e72181a63ab8cd58117511ced253cae5a8d71
Benjamin Louis <benjaminlouis.pro@gmail.com>
2019-04-05 16:04:48 +0200

    adds function to visualise numerics variables

---
+++ R/vis_numerics.R
@@ -0,0 +1,40 @@
+#' Get and Visualise distribution of a numeric vector
+#'
+#' @param .num a numeric vector
+#' @param .labx label of x-axix
+#' @param .bins bins argument for \code{geom_histogram}
+#' @param .plot if \code{TRUE} the plot is displayed
+#'
+#' @return a ggplot object
+#' @export
+#'
+#' @importFrom ggplot2 ggplot geom_histogram aes labs theme_classic theme element_text scale_y_continuous scale_fill_viridis_d
+#' @importFrom graphics plot
+#'
+#' @examples
+#' vis_numerics(rnorm(100))
+vis_numerics <- function(.num, .labx = "", .bins = .get_bins(.num), .plot = TRUE) {
+  # Remove NAs
+  .num <- .num[!is.na(.num)]
+  # Get the plot
+  ggp <- ggplot() +
+    geom_histogram(aes(.num, fill = ".num"), show.legend = FALSE,
+                   bins = .bins, boundary = min(.num), color = "#555555") +
+    labs(x = .labx, y = "Count") +
+    theme_classic() +
+    theme(axis.title = element_text(face = "bold", size = 12),
+          axis.text = element_text(face = "bold", size = 10))  +
+    scale_y_continuous(limits = c(0, NA), expand = c(0, 0)) +
+    scale_fill_viridis_d(option = "E")
+  # plot
+  if (.plot) { plot(ggp) }
+  # Return plot
+  return(ggp)
+}
+
+
+
+
+
+
+
---
+++ R/vis_numerics_all.R
@@ -0,0 +1,25 @@
+#' Get and Visualise distribution of all numeric column in a data frame
+#'
+#' @param .data a data frame
+#' @param .plot if \code{TRUE} the plot is displayed
+#'
+#' @return a list of ggplot objects
+#' @export
+#'
+#' @importFrom dplyr select_if
+#' @importFrom graphics plot
+#'
+#' @examples
+#' vis_numerics(rnorm(100))
+vis_numerics_all <- function(.data, .plot = TRUE) {
+  # Select numeric columns
+  numdata <- select_if(.data, is.numeric)
+  # There should be numeric colmns
+  if (ncol(numdata) == 0) {stop("No numeric columns found")}
+  # Get the distr
+  ggplist <- mapply(vis_numerics, .num = numdata, .labx = names(numdata), SIMPLIFY = FALSE)
+  # plot
+  if (.plot) { lapply(ggp, plot) }
+  # return the distr
+  return(ggplist)
+}
