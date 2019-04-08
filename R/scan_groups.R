997d7e500b50d6dff2cb162f0daf5d050efe38d4
Benjamin Louis <benjaminlouis.pro@gmail.com>
2019-04-08 17:02:22 +0200

    adds scan_groups function

---
+++ R/scan_groups.R
@@ -0,0 +1,35 @@
+#' Gives some statistics about categorical variables in a dataframe
+#'
+#' @param .data a data.frame
+#'
+#' @return a data.frame with one row for each group in each categorical variables
+#' in \code{data} with \code{variables}, the categorical varibales names, \code{groups}
+#' the groups names of categorical variables, \code{n} the occurence number for each
+#' groups and \code{p} the proportion
+#'
+#' @export
+#'
+#' @importFrom dplyr select_if %>% group_by count mutate
+#' @importFrom tidyr gather
+#'
+#' @examples
+#' data(iris)
+#' scan_groups(iris)
+scan_groups <- function(.data) {
+
+  # Selection of numeric variables
+  catdata <- select_if(.data, ~!is.numeric(.x))
+  # There should be numeric variables
+  if (ncol(catdata) == 0) {stop("No categorical variables found")}
+
+  #Stats
+  catdata <- catdata %>%
+    gather(key = "variables", value = "groups") %>%
+    group_by(variables) %>%
+    count(groups, sort = TRUE) %>%
+    mutate(p = n/sum(n))
+
+  #return
+  return(catdata)
+
+}
