93ed449e35b407bf99041f0fd242e86203d3ac58
Benjamin Louis <benjaminlouis.pro@gmail.com>
2019-04-10 19:18:38 +0200

    adds global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,2 @@
-globalVariables(c("variables", "groups", "n", "Cat", "Cat1", "Cat2", "Num", "cramerV", "r", "varia", "Freq", "Num1", "Num2", "Corr"))
+globalVariables(c("variables", "groups", "n", "Cat", "Cat1", "Cat2", "Num", "cramerV", "r", "varia", "Freq", "Num1", "Num2", "Corr",
+                  "Groups", "data", "Var", "Median", "value"))
