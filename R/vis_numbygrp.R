1e2d0d7119c00b52f2c33415c3e7334d1ab3e3a1
BenjaminLouis <benjaminlouis.pro@gmail.com>
2019-04-11 17:07:35 +0200

    removes useless code

--- R/vis_numbygrp.R
+++ R/vis_numbygrp.R
@@ -26,7 +26,6 @@ vis_numbygrp <- function(.cat, .num, .labx = "", .laby = "") {
     geom_violin(aes(reorder(.cat, .num, median, na.rm = TRUE), .num, fill = med), show.legend = FALSE,
                 draw_quantiles = 0.5, color = "#555555", na.rm = TRUE) +
     labs(x = .labx, y = .laby) +
-    labs() +
     theme(axis.title = element_text(face = "bold", size = 12),
           axis.text = element_text(face = "bold", size = 10))  +
     scale_y_continuous(limits = c(0, NA), expand = c(0, 0)) +
