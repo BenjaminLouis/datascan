3f73b4f86c9acead9607aaf9361ccf5f5a9ea8c5
Benjamin Louis <benjaminlouis.pro@gmail.com>
2019-04-12 10:22:34 +0200

    Adds roxygen skeleton

--- R/vis_groupsbygroups.R
+++ R/vis_groupsbygroups.R
@@ -1,3 +1,19 @@
+#' Visualise frequency of paired group between categorical variables in a data frame
+#'
+#' @param .data a data frame
+#' @param .regroup if \code{TRUE}, groups xith less than 5 occurences are grouped together under the name "Others_groups"
+#'
+#' @return a list of ggplot
+#' @export
+#'
+#' @importFrom dplyr select_if mutate_at select filter transmute pull
+#' @importFrom ggplot2 vars syms
+#' @importFrom purrr map2
+#' @importFrom tidyr crossing
+#'
+#' @examples
+#' library(dplyr)
+#' vis_groupsbygroups(starwars)
 vis_groupsbygroups <- function(.data, .regroup = TRUE) {
 
   catname <- select_if(.data, ~!is.numeric(.x) & !is.list(.x)) %>%
@@ -9,16 +25,17 @@ vis_groupsbygroups <- function(.data, .regroup = TRUE) {
       mutate_at(vars(!!!syms(catname)), .transform_cat)
   }
 
-  if (length(catname) <2) {
+  if (length(catname) < 2) {
     stop("There should be at least two categorical variables")
   }
 
-  df <- .data
+  df <- select(.data, !!!syms(catname))
   crossing(Cat1 = catname, Cat2 = catname) %>%
     filter(Cat1 != Cat2) %>%
     transmute(ggplist = map2(Cat1, Cat2, ~vis_grpbygrp(pull(df, .x),
                                    pull(df, .y),
-                                   .x, .y))) %>%
+                                   .labx = .x,
+                                   .laby = .y))) %>%
     pull(ggplist)
 
 }
