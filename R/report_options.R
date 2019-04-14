d31f4f65f2432a1dfa60a9ab3b7a4581c0975d25
BenjaminLouis <benjaminlouis.pro@gmail.com>
2019-04-14 12:14:26 +0200

    adds config report function

---
+++ R/report_options.R
@@ -0,0 +1,12 @@
+#' Config options for automatic report
+#'
+#' @param .title report title
+#' @param .author report author
+#' @param .date report date
+#'
+#' @return a list of config values
+#' @export
+report_options <- function(.title = "Exploratory Data Analysis report",
+                           .author = "John Doe", .date = Sys.Date()) {
+  list(title = .title, author = .author, date = .date)
+}
