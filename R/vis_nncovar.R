87eddeb3da67acea2d4d7253ab5818cadab3a5cf
Benjamin Louis <benjaminlouis.pro@gmail.com>
2019-04-15 21:24:26 +0200

    Features : vis_nncovar take into account colors by groups

--- R/vis_nncovar.R
+++ R/vis_nncovar.R
@@ -2,13 +2,14 @@
 #'
 #' @param .data a data frame
 #' @param ... you can use this arguement to specify targeted numerical columns that will be used as y-axis variable
+#' @param .by color scatter by groups
 #'
 #' @return a list of ggplots
 #' @export
 #'
-#' @importFrom dplyr select_if setdiff mutate select pull
+#' @importFrom dplyr select_if setdiff mutate select pull tibble
 #' @importFrom ggplot2 quos quo_name sym
-#' @importFrom purrr map2 map map_chr
+#' @importFrom purrr map2 map map_chr pmap
 #' @importFrom tidyr crossing
 #' @importFrom utils combn
 #'
@@ -18,11 +19,12 @@
 #'
 #' # With a targeted y-axis variable
 #' vis_nncovar(iris, Sepal.Length)
-vis_nncovar <- function(.data, ...) {
+vis_nncovar <- function(.data, ..., .by = NULL) {
 
   target <- quos(...)
   target_name <- map_chr(target, quo_name)
 
+  dt <- .data
   df <- select_if(.data, is.numeric)
   non_target_name <- setdiff(colnames(df), target_name)
 
@@ -37,15 +39,26 @@ vis_nncovar <- function(.data, ...) {
   }
 
   if (length(target_name) > 0) {
-    dfcross <- crossing(x = non_target_name, y = target_name) %>%
-      mutate(data = map2(x, y, ~select(df, .x, .y))) %>%
-      pull(data)
+    dfcross <- crossing(x = non_target_name, y = target_name, by = .by)
   } else {
-    dfcross <- combn(non_target_name, 2, simplify = FALSE) %>%
-      map(select, .data = df)
+    comb <- combn(non_target_name, 2, simplify = FALSE)
+    dfcross <- crossing(dt = 1:length(comb), by = .by)  %>%
+      mutate(dt = map(dt, ~tibble(x = comb[[.x]][1], y = comb[[.x]][2]))) %>%
+      unnest(dt) %>%
+      select(x, y, everything())
   }
 
-  dfcross %>%
-    map(~plot_scatter(.x, .num1 = !!sym(names(.x)[1]), .num2 = !!sym(names(.x)[2])))
+  if (is.null(.by)) {
+    dfcross %>%
+      mutate(data = map2(x, y, ~select(dt, .x, .y))) %>%
+      pull(data) %>%
+      map(~plot_scatter(.x, .num1 = !!sym(names(.x)[1]), .num2 = !!sym(names(.x)[2])))
+  } else {
+    dfcross %>%
+      mutate(data = pmap(list(x, y, by), ~select(dt, ..1, ..2, ..3))) %>%
+      pull(data) %>%
+      map(~plot_scatter(.x, .num1 = !!sym(names(.x)[1]), .num2 = !!sym(names(.x)[2]), .by = !!sym(names(.x)[3])))
+  }
+
 
 }
