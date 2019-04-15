b498f5d64592c61ffdfde44c29f5f721608d23c3
BenjaminLouis <benjaminlouis.pro@gmail.com>
2019-04-15 18:23:47 +0200

    updates template

--- R/report_options.R
+++ R/report_options.R
@@ -3,10 +3,13 @@
 #' @param .title report title
 #' @param .author report author
 #' @param .date report date
+#' @param .regroup logical. Should the groups with less than 5 occurences be grouped together.
+#' @param .target columns names of variables to be considered as response.
 #'
 #' @return a list of config values
 #' @export
 report_options <- function(.title = "Exploratory Data Analysis report",
-                           .author = "John Doe", .date = Sys.Date()) {
+                           .author = "John Doe", .date = Sys.Date(),
+                           .regroup = TRUE, .target = NULL) {
   list(title = .title, author = .author, date = .date)
 }
