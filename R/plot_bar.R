a6468789ed1188c28a8157ed4dc59deed8eb4249
Benjamin Louis <benjaminlouis.pro@gmail.com>
2019-05-07 15:36:58 +0200

    makes possible a percent scale

--- R/plot_bar.R
+++ R/plot_bar.R
@@ -4,6 +4,7 @@
 #' @param .cat unquoted name of a categrocial column
 #' @param .by (optional) unquoted name of a categrocial column
 #' @param nas logical. Should missing values be considered as group on x-axis
+#' @param bytype only if \code{.by} argument is given. Either "count" or "percent" for y-axis
 #'
 #' @return a ggplot
 #' @export
@@ -17,7 +18,7 @@
 #' @examples
 #' library(dplyr)
 #' plot_bar(starwars, eye_color)
-plot_bar <- function(.data, .cat, .by, nas = TRUE) {
+plot_bar <- function(.data, .cat, .by, nas = TRUE, bytype = "count") {
 
   var <- enquo(.cat)
 
@@ -43,6 +44,8 @@ plot_bar <- function(.data, .cat, .by, nas = TRUE) {
     textsize <- 5
   }
 
+
+
   #Flip coordinate if there is more than 5 groups
   if (ngrp > 5) {
     if (missing(.by)) {
@@ -58,7 +61,14 @@ plot_bar <- function(.data, .cat, .by, nas = TRUE) {
     } else {
       ggp <- df %>%
         count(!!var, !!by, .drop = FALSE) %>%
-        complete(!!var, !!by, fill = list(n = 0)) %>%
+        complete(!!var, !!by, fill = list(n = 0))
+      if (bytype == "percent") {
+        ggp <- ggp %>%
+          group_by(!!by) %>%
+          mutate(n = n/sum(n)) %>%
+          ungroup()
+      }
+      ggp <- ggp %>%
         ggplot(aes(x = !!by, y = n, fill = !!var)) +
         geom_bar(stat = "identity", color = "#555555", position = "dodge") +
         coord_flip() +
@@ -78,9 +88,16 @@ plot_bar <- function(.data, .cat, .by, nas = TRUE) {
             axis.text.x = element_text(face = "bold", size = textsize),
             axis.text.y = element_text(face = "bold", size = 10))
     } else {
-      ggp <-  df %>%
+      ggp <- df %>%
         count(!!var, !!by, .drop = FALSE) %>%
-        complete(!!var, !!by, fill = list(n = 0)) %>%
+        complete(!!var, !!by, fill = list(n = 0))
+      if (bytype == "percent") {
+        ggp <- ggp %>%
+          group_by(!!by) %>%
+          mutate(n = n/sum(n)) %>%
+          ungroup()
+      }
+      ggp <- ggp %>%
         ggplot(aes(x = !!by, y = n, fill = !!var)) +
         geom_bar(stat = "identity", color = "#555555", position = "dodge") +
         theme_classic() +
@@ -90,11 +107,17 @@ plot_bar <- function(.data, .cat, .by, nas = TRUE) {
     }
   }
 
-  ggp +
-    labs(x = quo_name(var), y = "Count")  +
-    scale_y_continuous(limits = c(0, NA), expand = c(0, 0)) +
+  ggp <- ggp +
+    labs(x = quo_name(var), y = sub("^.", toupper(substr(bytype, 1, 1)), bytype))  +
     scale_x_discrete(na.translate = nas) +
     scale_fill_viridis_d()
+  if (bytype == "percent") {
+    ggp +
+      scale_y_continuous(limits = c(0, NA), expand = c(0, 0), labels = function(x) paste0(x*100, "%"))
+  } else {
+    ggp +
+      scale_y_continuous(limits = c(0, NA), expand = c(0, 0))
+  }
 
 
 }
